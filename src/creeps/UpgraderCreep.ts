5a9c6646665f35bbbf70d9ac1a499c794eb1fd3a
Andrew Wessels <shixish@gmail.com>
2022-01-16 05:59:01 -1000

    max of 5 upgraders

--- src/creeps/UpgraderCreep.ts
+++ src/creeps/UpgraderCreep.ts
@@ -35,7 +35,7 @@ export class UpgraderCreep extends BasicCreep {
         ],
         max: (roomAudit:RoomAudit)=>{
           if (!roomAudit.controller?.containers.length) return 0;
-          return Math.max(2 + roomAudit.flags.harvest.length*2, 6);
+          return Math.max(2 + roomAudit.flags.harvest.length*2, 5);
         }
       },
       {
